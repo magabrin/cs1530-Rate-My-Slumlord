f95928eea6db16b0fc7737644819a5ee38610817
magabrin <mag313@pitt.edu>
2019-07-25 10:44:22 -0400

    wip continuing to spice up the home page

--- src/main/js/components/Home.js
+++ src/main/js/components/Home.js
@@ -35,7 +35,7 @@ class Home extends React.Component {
                                 <div class="card-header">Login</div>
                                 <div class="card-body">
                                     <h5 class="card-title">Login To Your Account</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <p class="card-text">Creating an account allows you to create reviews, so you can contribute with your tenant experience.</p>
                                 </div>  
                             </div>
                             </a>
@@ -45,12 +45,12 @@ class Home extends React.Component {
                             <div class="card text-white bg-secondary mb-3" style={{maxWidth: '26rem', height: '20rem'}}>
                                 <div class="card-header">Landlords</div>
                                 <div class="card-body">
-                                    <h5 class="card-title">View Landlord Page</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <h5 class="card-title">View Landlord Pages</h5>
+                                    <p class="card-text">See the reviews for landlords. Create a review if you are logged in. Search for a landlord.</p>
                                     </div>
                             </div>
                             </a>
-                        </div>                        
+                        </div>                      
                     </div>
                     <div class="row"></div>
                     <div class="row">
@@ -60,7 +60,7 @@ class Home extends React.Component {
                                 <div class="card-header">Properties</div>
                                 <div class="card-body">
                                     <h5 class="card-title">See All Listed Properties</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <p class="card-text">See a list of all properteis with reviews. Search for reviews based on the property.</p>
                                 </div>  
                             </div>
                             </a>
@@ -71,25 +71,29 @@ class Home extends React.Component {
                                 <div class="card-header">Tenant laws</div>
                                 <div class="card-body">
                                     <h5 class="card-title">See Common Tenant Laws</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <p class="card-text">Your quick link for tenant/landlord faqs. See some common laws that every tenant should be aware of, especially if you're a first time renter.</p>
                                 </div>
                             </div>
                             </a>
-                        </div>                        
+                        </div> 
+                                                
                     </div>
 
-                    <div class="row">
+                    {/* <div class="row">
                         <h3>Top Properties</h3>                        
                     </div>
                     <div class="row">
                         {this.state.topProperties === undefined ? <div>No Properties</div> : <div>Display Properties here</div> }
-                    </div>
-                    <div class="row">
-                        <h3>Top Landlords</h3>                        
-                    </div>
-                    <div class="row">
-                        {this.state.topLandlords === undefined ? <div>No Landlords</div> : <div>Display landlords here</div> }
-                    </div>
+                    </div> */
+                    }
+                   <div class="col-lg">
+                        <div class="row">
+                            <h3>Top Landlords</h3>                        
+                        </div>
+                        <div class="row">
+                            {this.state.topLandlords === undefined ? <div>No Landlords</div> : <div>Display landlords here</div> }
+                        </div>
+                    </div>  
                     
                     
 
